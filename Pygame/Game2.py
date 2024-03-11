5e582d701bb7c7fad81bd5e8006fcbd01f894873
Nontain <112168676+Nontain@users.noreply.github.com>
2024-03-11 16:20:46 +0200

    Update Game2.py

--- Pygame/Game2.py
+++ Pygame/Game2.py
@@ -1,20 +1,9 @@
 from pygame import *
 
-mixer.init()
-mixer.music.load("blud.mp3") 
-mixer.music.play()
-kick = mixer.Sound("blud.mp3")
-kick.play()
-window = display.set_mode((700, 500))
-display.set_caption("catch")
-background = transform.scale(image.load("C:\\Users\\BB\\Documents\\GitHub\\PyQt\demos\\background.jpg"), (700, 250))
-
 x1 = 100
 y1 = 300
 x2 = 300
 y2 = 300
-sprite1 = transform.scale(image.load('sprite1.png'), (100, 100))
-sprite2 = transform.scale(image.load('sprite2.png'), (100, 100))
 speed = 10
 #game loop
 
@@ -23,10 +12,8 @@ clock = time.Clock()
 FPS = 144
 white = (255,255,255) 
 while run:
-    window.fill((white))
-    window.blit(background,(0, 0))
-    window.blit(sprite1, (x1, y1))
-    window.blit(sprite2, (x2, y2))
+
+
 
     for e in event.get():
         if e.type == QUIT:
